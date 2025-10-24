c848dcc28c1f844201ce5cd6231e8e16756f8025
Sameni Narayan Rao <53487866+SameniNarayanRao@users.noreply.github.com>
2025-10-24 16:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = pickle.load(open("randomforest.pkl", "rb"))
 
 @app.route('/')
 def main():
-    return render('index.html')
+    return <a href="index.html"
 
 @app.route('/predict', methods = ['POST'])
 def index():
@@ -18,12 +18,13 @@ def index():
     Weight = request.form['d']
     array = np.array([[Age, Sex, Height, Weight]])
     pred = model.predict(array)
-    return render("result.html", data = pred)
+    return <a href="result.html">, data = pred)
 
 
 
 if __name__ == "__main__":
     app.run(debug = True)
+
 
 
 
