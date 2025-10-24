2a820a77c0bba696f8f27bea4ddd1cc81d7560be
Sameni Narayan Rao <53487866+SameniNarayanRao@users.noreply.github.com>
2025-10-24 16:18:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask,
 import pickle
 import numpy as np
 
@@ -8,7 +8,7 @@ model = pickle.load(open("randomforest.pkl", "rb"))
 
 @app.route('/')
 def main():
-    return <a href="index.html"
+    return <a href='index.html'
 
 @app.route('/predict', methods = ['POST'])
 def index():
@@ -18,12 +18,13 @@ def index():
     Weight = request.form['d']
     array = np.array([[Age, Sex, Height, Weight]])
     pred = model.predict(array)
-    return <a href="result.html">, data = pred)
+    return <a href='result.html'>, data = pred)
 
 
 
 if __name__ == "__main__":
     app.run(debug = True)
+
 
 
 
